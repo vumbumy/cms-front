e8c9403e91108eac5dbb49905498add8ffafb647
HanBeom Seo <vumbumy@outlook.kr>
2020-12-30 16:00:03 +0900

    FIx border left to change color on "TopContents"

--- src/components/TopContents.vue
+++ src/components/TopContents.vue
@@ -1,22 +1,24 @@
 <template>
-    <!-- TODO: 컬러 보더 추가 -->
-    <v-sheet class="pl-4 my-2 d-flex" style="border-left: 10px solid orange;">
-        <div class="d-flex flex-column text-left">
-            <div v-if="readonly" class="text-h5 text-sm-h4">
-                <strong v-text="item.name"/>
+    <div class="d-flex ">
+        <div :class="colors[colorIndex]" style="width: 10px;" @click="colorIndex = (colorIndex + 1) % colors.length"/>
+        <v-sheet class="pl-4 my-2 d-flex flex-grow-1">
+            <div class="d-flex flex-column text-left">
+                <div v-if="readonly" class="text-h5 text-sm-h4">
+                    <strong v-text="item.name"/>
+                </div>
+                <v-text-field v-else label="이름" v-model="item.name"/>
+                <div v-if="readonly" class="text-h6 text-sm-h5 grey--text text--darken-2">
+                    <strong v-text="item.type"/>
+                </div>
+                <v-text-field v-else label="카테고리" v-model="item.type"/>
             </div>
-            <v-text-field v-else label="이름" v-model="item.name"/>
-            <div v-if="readonly" class="text-h6 text-sm-h5 grey--text text--darken-2">
-                <strong v-text="item.type"/>
+            <div class="ml-auto d-flex flex-column text-right">
+                <div class="caption" v-text="item.updated"/>
+                <div class="caption grey--text text--darken-2" v-text="item.created"/>
+                <permissions class="mt-auto"/>
             </div>
-            <v-text-field v-else label="카테고리" v-model="item.type"/>
-        </div>
-        <div class="ml-auto d-flex flex-column text-right">
-            <div class="caption" v-text="item.updated"/>
-            <div class="caption grey--text text--darken-2" v-text="item.created"/>
-            <permissions class="mt-auto"/>
-        </div>
-    </v-sheet>
+        </v-sheet>
+    </div>
 </template>
 
 <script>
@@ -29,6 +31,8 @@
             readonly: Boolean
         },
         data: () => ({
+            colorIndex: 0,
+            colors: ["orange", "grey"],
             item: {
                 name: "디지털01-의정부",
                 type: "나임의 정부",
